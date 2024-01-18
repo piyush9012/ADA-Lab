c527cfd39c039d85d6225e9cd8bbb54cd6c42761
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-01-18 21:42:47 +0530

    merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,71 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-private:
-    static void merge(int arr[], int low, int mid, int high) {
-        vector<int> temp;      // temporary array
-        int left = low;        // starting index of the left half of arr
-        int right = mid + 1;   // starting index of the right half of arr
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp;          // temporary array
+    int left = low;            // starting index of left half of arr
+    int right = mid + 1;       // starting index of right half of arr
 
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                temp.push_back(arr[left]);
-                left++;
-            } else {
-                temp.push_back(arr[right]);
-                right++;
-            }
-        }
+    //storing elements in the temporary array in a sorted manner//
 
-        while (left <= mid) {
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
             temp.push_back(arr[left]);
             left++;
         }
-
-        while (right <= high) {
+        else {
             temp.push_back(arr[right]);
             right++;
         }
-
-        for (int i = low; i <= high; i++) {
-            arr[i] = temp[i - low];
-        }
     }
 
-public:
-    static void mergeSort(int arr[], int low, int high) {
-        if (low >= high)
-            return;
-
-        int mid = (low + high) / 2;
-        mergeSort(arr, low, mid);      // left half
-        mergeSort(arr, mid + 1, high); // right half
-        merge(arr, low, mid, high);    // merging sorted halves
+    while (left <= mid) {     // if elements on the left half are still left //
+        temp.push_back(arr[left]);
+        left++;
     }
-};
+
+    while (right <= high) {    //  if elements on the right half are still left //
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
+}
 
 int main() {
-    auto start = chrono::high_resolution_clock::now();
-    int arr[] = {9, 4, 7, 6, 3, 1, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Before sorting array: ";
+    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+    int n = 7;
+
+    cout << "Before Sorting Array: " << endl;
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        cout << arr[i] << " "  ;
     }
-
-    Solution::mergeSort(arr, 0, n - 1);
-
-    cout << "\n\nAfter sorting array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    auto end = chrono::high_resolution_clock::now();
-    auto executeTime = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    cout << "\n\nMerge sort takes: " << executeTime << "ms" << endl;
-
-    return 0;
+    cout << endl;
+    mergeSort(arr, 0, n - 1);
+    cout << "After Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    return 0 ;
 }
