199f64ceb6509c3e8809a0175e8915ad0a19086c
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-01-22 22:16:04 +0530

    Selection Sort

--- 1b_selection_sort.cpp
+++ 1b_selection_sort.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
-#include <chrono>
+#include <bits/stdc++.h>
 using namespace std;
 
 void selection_sort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i<n-1; i++) {
         int min = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min]) {
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min])
                 min = j;
-            }
         }
-        int temp = arr[min];  // swap
+        int temp = arr[min];
         arr[min] = arr[i];
         arr[i] = temp;
     }
-    cout << "\n\nAfter selection sort:";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
 }
 
 int main() {
@@ -24,15 +19,18 @@ int main() {
     int arr[] = {13, 46, 24, 52, 20, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Before selection sort:";
+    cout << "Before selection sort: ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
 
     selection_sort(arr, n);
 
+    cout <<"\nAfter selection sort: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
     auto end = chrono::high_resolution_clock::now();
     auto execute = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    cout << "\nSelection sort takes: " << execute << "ms" << endl;
-
+    cout << "\nSelection sort takes: " << execute << " ms" << endl;
     return 0;
 }
