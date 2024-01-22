ea0f06321bbcb0eb19d08923e6e4b1a7f2a9d4f0
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-01-22 22:09:46 +0530

    dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,70 +1,49 @@
-/*A DFS based program in which outputs all systems reachable from a given system and also its time and space complexity for a network having ‘n’ systems.*/
-#include <iostream>
-#include <vector>
-#include <chrono>
-#include <unordered_set>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-class Network
-{
-    int n;                   // Number of systems
-    vector<vector<int>> adj; // Adjacency list
-public:
-    Network(int numSystems) : n(numSystems) {
-        adj.resize(n); // Initialize adjacency list for n systems
-    }
-
-    // Function to add an edge between systems
-    void addEdge(int u, int v) {
-        adj[u].push_back(v);
-        // For undirected graph, add the line below to make it bidirectional
-        // adj[v].push_back(u);
-    }
-
-    // Depth-First Search traversal to find reachable systems
-    void DFS(int start, vector<bool> &visited) {
-        visited[start] = true; // Mark the current system as visited
-        cout << start << " ";  // Output the current system (reachable)
-
-        // Traverse all systems adjacent to the current system
-        for (int u : adj[start]) {
-            if (!visited[u]) {
-                DFS(u, visited); // Recursive DFS call for unvisited systems
+class Solution {
+  private: 
+    void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
+        vis[node] = 1; 
+        ls.push_back(node);
+        for(auto i : adj[node]) {    // traverse all its neighbours
+            if(!vis[i]) {            // if the neighbour is not visited
+                dfs(i, adj, vis, ls); 
             }
         }
     }
-
-    // Function to find reachable systems from a given system
-    void findReachableSystems(int start) {
-        vector<bool> visited(n, false); // Array to mark visited systems
-
-        cout << "Systems reachable from system " << start << ": ";
-        DFS(start, visited);
-        cout << endl;
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        int vis[V] = {0}; 
+        int start = 0;
+        vector<int> ls;     // create a list to store dfs
+        dfs(start, adj, vis, ls);    // call dfs for starting node
+        return ls; 
     }
 };
 
+void addEdge(vector <int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printAns(vector <int> &ans) {
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+}
+
 int main() {
-    int numSystems = 6; // Number of systems in the network
-    Network network(numSystems);
+    vector <int> adj[5];
+    
+    addEdge(adj, 0, 2);
+    addEdge(adj, 2, 4);
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 3);
 
-    // Adding edges between systems
-    network.addEdge(0, 1);
-    network.addEdge(0, 2);
-    network.addEdge(1, 3);
-    network.addEdge(1, 4);
-    network.addEdge(2, 4);
-    network.addEdge(3, 5);
-    network.addEdge(4, 5);
-
-    int startSystem = 0;     // Choose the starting system for DFS traversal
-    clock_t start,end;
-    start=clock();
-    network.findReachableSystems(startSystem);
-    end=clock();
-    double time_spent = (double)(end-start)/CLOCKS_PER_SEC;
-    cout<<"\ntime spent: "<<time_spent;
-
+    Solution obj;
+    vector <int> ans = obj.dfsOfGraph(5, adj);
+    printAns(ans);
     return 0;
 }
