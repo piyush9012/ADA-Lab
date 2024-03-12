a68f9c9b602e4b6c49fcb11d6eee030c7504aeb1
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-03-12 19:16:18 +0530

    Kruskal's Algorithm

---
+++ 9_kruskal.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cost[10][10], parent[10], a, b, n;
+
+void find_min() {
+    int min = 999;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            if (cost[i][j] < min) {
+                min = cost[i][j];
+                a = i;
+                b = j;
+            }
+}
+
+int check_cycle(int a, int b) {
+    if ((parent[a] == parent[b]) && (parent[a] != 0))
+        return 0;
+    else if (parent[a] == 0 && parent[b] == 0)
+        parent[a] = parent[b] = a;
+    else if (parent[a] == 0)
+        parent[a] = parent[b];
+    else if (parent[a] != parent[b])
+        parent[b] = parent[a];
+    return 1;
+}
+
+int main()
+{
+    int min, count = 0, sum = 0, flag = 0;
+    cout << "Enter the number of vertices: ";
+    cout <<n;
+    cout << "\nEnter the cost matria (Enter 999 for No edges and self loops):\n";
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++) {
+            cin >> cost[i][j];
+            parent[j] = 0;
+        }
+    while (count != n - 1 && min != 999) {
+        find_min();
+        flag = check_cycle(a, b);
+        if (flag == 1) {
+            cout << a <<" --> " << b <<" = " << cost[a][b];
+            count++;
+            sum += cost[a][b];
+        }
+        cost[a][b] = cost[b][a] = 999;
+    }
+    cout << "The sum cost of minimum spanning tree = "<< sum;
+    return 0;
+}
